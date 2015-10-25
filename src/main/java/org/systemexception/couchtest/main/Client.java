10fa38425eb20f56c6dab6400e1ffadf986748cc
Leonardo Cappuccio <leo@systemexception.org>
2015-10-25 21:20:19 +0100

    use _all_docs view extracts all data

---
+++ src/main/java/org/systemexception/couchtest/main/Client.java
@@ -0,0 +1,42 @@
+package org.systemexception.couchtest.main;
+
+import org.lightcouch.CouchDbClient;
+import org.systemexception.couchtest.pojo.Data;
+
+import java.io.*;
+import java.util.List;
+
+/**
+ * @author leo
+ * @date 25/10/15 21:04
+ */
+public class Client {
+
+	public static void main(String[] args) throws IOException {
+		CouchDbClient dbClient = new CouchDbClient("test_replicate", //dbName
+				false,  //create db if not exists
+				"http", //protocol, http or https
+				"192.168.1.27", //obvious
+				5984,   //port
+				null,   //username
+				null   //password
+		);
+
+		List<Data> dataList = dbClient.view("_all_docs").query(Data.class);
+
+		System.out.println("Found " + dataList.size() + " items");
+
+		for (Data data: dataList) {
+			String dataUuid = data.getDataUUID();
+			InputStream dataStream = data.getInputStream();
+			byte[] buffer = new byte[dataStream.available()];
+			dataStream.read(buffer);
+			File targetFile = new File("target" + File.separator + dataUuid + ".png");
+			OutputStream outStream = new FileOutputStream(targetFile);
+			outStream.write(buffer);
+			dataStream.close();
+			outStream.close();
+			System.out.println(data.getDataUUID());
+		}
+	}
+}
